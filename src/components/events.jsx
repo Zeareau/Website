8b4ed57e2f7688b122dfef206d767854a6ff5662
Christopher Enriquez <zeareau@Christophers-MacBook-Pro-2.local>
2025-11-25 12:55:25 -0500

    clearing calendar

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -1,11 +1,11 @@
 // src/data/events.js
 export const EVENTS = [
-  {
-    date: "Nov 13, 2025",
-    title: "GBM 3 - Business Analytics - CGI",
-    where: "Duques 453 - 6:15pm",
-    blurb: "Work with a CGI Business Analyst to analyze real business data.",
-    tags: ["CGI", "Data Visualization", "Client-Work"],
+ {
+    date: "---",
+    title: "TBD",
+    where: "---",
+    blurb: "---",
+    tags: [],
   },
   {
     date: "---",
